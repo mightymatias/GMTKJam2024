39f1c8c998cbd8703bad92fb01312a24308c15ca
Austin Matias <austinjmatias@gmail.com>
2024-08-18 22:29:06 -0400

    Combinations work!!
    You must build from bottom up, but yes! Combinations now work!

--- Assets/Scripts/CombinationStation.cs
+++ Assets/Scripts/CombinationStation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CombinationStation : InteractionStation
@@ -18,9 +19,18 @@ public class CombinationStation : InteractionStation
         // tier 0 is two normal crumbs. Every tier after is a crumb and a complex crumb
         if (currentTier > 0){
             Crumb combinedCrumb = TryToCombine(crumb, lastCombination);
+            if (combinedCrumb != null){
+                Destroy(crumb.gameObject);
+                Destroy(lastCombination.gameObject);
+                Instantiate(combinedCrumb.GameObject(), transform.Find("Interact Position").position, Quaternion.identity);
+                lastCombination = null;
+                currentTier = 0;
+            }
         } else {
-            crumb = lastCombination;
+            lastCombination = crumb;
+            currentTier++;
         }
+        
         
         
     }
@@ -30,20 +40,35 @@ public class CombinationStation : InteractionStation
     }
 
     public override bool CanInteractWithCrumb(Crumb crumb){
-        foreach (int tier in crumb.combinationTiers){
-            Debug.Log("**!!**!!Crumb Tier: " + tier + "|Station tier: " + currentTier);
-            if (tier == currentTier){
-                Debug.Log("Can Interact");
-                currentTier++;
-                return true;
+        Debug.Log("CURRENT TIER: " + currentTier);
+        // if this is the first ingredient in (of 2), check all ingredient 1's to see if it's on the list
+        if (currentTier == 0){
+            Debug.Log(crumb.crumbName);
+            foreach (ComplexCrumb interaction in interactionPossibilities){
+                Debug.Log(interaction.ingredient1.GetComponent<Crumb>().crumbName);
+                if (interaction.ingredient1.GetComponent<Crumb>().crumbName == crumb.crumbName){
+                    Debug.Log("returning true ing1");
+                    return true;
+                }
             }
+            return false;
+        } else { // else this is ingredient 2, and we do the same thing, but with ingredient 2's
+            foreach (ComplexCrumb interaction in interactionPossibilities){
+                Debug.Log("..searching ingredient 2");
+                if (interaction.ingredient2.GetComponent<Crumb>().crumbName == crumb.crumbName){
+                    Debug.Log("returning true ing2");
+                    return true;
+                }
+            }
+            return false;
         }
-        return false;
     }
 
     public Crumb TryToCombine(Crumb crumb, Crumb previousCrumb){
+        Debug.Log(previousCrumb.crumbName + " | " + crumb.crumbName);
         foreach(ComplexCrumb interaction in interactionPossibilities){
-            if (interaction.ingredient1 == previousCrumb && interaction.ingredient2 == crumb){
+            Debug.Log(interaction + " is made from " + interaction.ingredient1.GetComponent<Crumb>().crumbName + " and " + interaction.ingredient2.GetComponent<Crumb>().crumbName);
+            if (interaction.ingredient1.GetComponent<Crumb>().crumbName == previousCrumb.crumbName && interaction.ingredient2.GetComponent<Crumb>().crumbName == crumb.crumbName){
                 return interaction;
             }
         }
